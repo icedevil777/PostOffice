3432dbe661677bacf62a80d0dff602b20a0ed899
icedevil777111 <icedevil777111@mail.ru>
2022-01-26 23:23:54 +0300

    Create app

---
+++ main.py
@@ -0,0 +1,22 @@
+# Initial data
+from typing import Tuple
+
+point_1 = (0, 2)  # Post office
+point_2 = (2, 5)  # Griboedova street
+point_3 = (5, 2)  # Backer street
+point_4 = (6, 6)  # Bolshaya Sadovaya street
+point_5 = (8, 3)  # Evergreen street
+
+
+# def tripgeneration(p_1, p_2):
+#     pass
+
+
+def getrange(p_1, p_2):
+    pass
+
+
+a = [10]
+for i in range(10):
+    a[i] = i
+print(a)
